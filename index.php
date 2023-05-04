1f44be8d0fb8ff52524a4db36c5633f4a2f75075
taddeoneri <taddeo.neri10@gmail.com>
2023-05-04 15:38:15 +0200

    table

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php 
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ]
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="d-flex align-items-center flex-column">
+    <h1 class="text-center py-3">Hotels PHP</h1>
+    <div>
+        <table class="table py-3">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for($i = 0; $i < count($hotels); $i++){ ?>
+                <tr>
+                    <td><?php echo $hotels[$i]['name']; ?></th>
+                    <td><?php echo $hotels[$i]['description']; ?></td>
+                    <td><?php echo $hotels[$i]['parking']; ?></td>
+                    <td><?php echo $hotels[$i]['vote']; ?></td>
+                    <td><?php echo $hotels[$i]['distance_to_center']; ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>    
+    </div>
+    
+</body>
+</html>
